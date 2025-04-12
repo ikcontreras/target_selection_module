569a197c789c514bab3a1e8af337090e23191bd3
ikcontreras <ikcontreras@outlook.es>
2025-04-12 22:19:49 +0200

    refactor: integrate types in sortEnemies

--- src/utils/sortEnemies.ts
+++ src/utils/sortEnemies.ts
@@ -1,11 +1,10 @@
-import { RadarPayload } from "@types";
+import { ScanPosition } from "@types";
 
-export function sortEnemies(radar: RadarPayload) {
-  return radar.scan.sort((a, b) => {
+export function sortEnemies(positions: Array<ScanPosition>) {
+  return positions.sort((a, b) => {
     return (
-      a.coordinates.x ** 2 +
-      a.coordinates.y ** 2 -
-      (b.coordinates.x ** 2 + b.coordinates.y ** 2)
+      Math.sqrt(a.coordinates.x ** 2 + a.coordinates.y ** 2) -
+      Math.sqrt(b.coordinates.x ** 2 + b.coordinates.y ** 2)
     );
   });
 }
