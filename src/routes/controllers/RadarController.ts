3c1b9fd8992340d734679ac2f3d566f9975cefff
ikcontreras <ikcontreras@outlook.es>
2025-04-12 22:21:42 +0200

    refactor: integrate protocols to radarController

--- src/routes/controllers/RadarController.ts
+++ src/routes/controllers/RadarController.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from "express";
-import { RadarPayload, Protocols, RadarResponse } from "@types";
+import { RadarPayload, RadarResponse } from "@types";
+import { protocolStrategies } from "@protocols";
 
 export function RadarController(
   req: Request<unknown, unknown, RadarPayload>,
   res: Response<RadarResponse>,
 ) {
-  const coordinates = req.body.scan[0].coordinates;
+  let positions = req.body.scan;
 
-  res.send(coordinates);
+  req.body.protocols.forEach((protocol) => {
+    const protocolStrategy = protocolStrategies.find((pf) =>
+      pf().condition(protocol),
+    );
+    if (protocolStrategy) positions = protocolStrategy().execute(positions);
+  });
+
+  res.send({
+    x: positions[0].coordinates.x,
+    y: positions[0].coordinates.y,
+  });
 }
